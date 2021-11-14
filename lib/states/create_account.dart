43cefb7a1f6d486d59b48b515363a8fdbe98f42d
jeun <jeun@spc.com>
2021-11-14 20:57:25 +0700

    update

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -8,8 +10,138 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  String? typeUser;
+  Row buildName(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'Name :',
+              prefixIcon: Icon(
+                Icons.fingerprint,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create New Account'),
+        backgroundColor: MyConstant.primary,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          buildTitle('ຂໍ້ມູນທົ່ວໄປ :'),
+          buildName(size),
+          buildTitle('ຊະນິດຂອງ  User :'),
+          buildRadioBuyer(size),
+          buildRadioSeller(size),
+          buildRadioRider(size),
+        ],
+      ),
+    );
+  }
+
+  Row buildRadioBuyer(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'buyer',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ຜູ້ຊື້ (Buyer)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildRadioSeller(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'seller',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ຜູ້ຂາຍ (Seller)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildRadioRider(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'rider',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ຜູ້ສົ່ງ (Rider)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildTitle(String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16),
+      child: ShowTitle(
+        title: title,
+        textStyle: MyConstant().h2Style(),
+      ),
+    );
   }
 }
